a8703f43f6c045506579ea3aad39c81c8f2f397c
plumvillagesongs <117699415+plumvillagesongs@users.noreply.github.com>
2023-03-29 00:33:05 +0700

    Update keyboard_chord_charts.dart
    Add defaultColor to display white note

--- lib/keyboard_chord_charts.dart
+++ lib/keyboard_chord_charts.dart
@@ -1,36 +1,38 @@
 // ignore_for_file: non_constant_identifier_names
 
 import 'dart:math';
-
 import 'package:flutter/material.dart';
 
 class KeyboardChordChart extends StatelessWidget {
   final List<Color?> colors;
+  final Color defaultColor;
 
   /// Builds a keyboard chord chart with the given colors.
   /// The list of colors has to be of length 24.
-  const KeyboardChordChart.fromColors(this.colors, {Key? key})
+  const KeyboardChordChart.fromColors(this.colors, this.defaultColor, {Key? key})
       : assert(colors.length == 24),
         super(key: key);
 
   KeyboardChordChart.fromNoteIndexes(Set<int> noteIndexes,
-      {Color highlightColor = Colors.blue, Key? key})
+      {Color highlightColor = Colors.blue,Color defaultColor = Colors.transparent, Key? key})
       : this.fromColors(
             List.generate(
                     24,
                     (index) =>
                         noteIndexes.contains(index) ? highlightColor : null)
                 .toList(),
+            defaultColor,
             key: key);
 
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(builder: (context, constraints) {
+      double ratio = 0.5;
       double maxWidth =
-          min(constraints.maxWidth, constraints.maxHeight * 168 / 50) * 0.9;
+          min(constraints.maxWidth, constraints.maxHeight * 168 / 50) * ratio;
       return CustomPaint(
         size: Size(maxWidth, maxWidth * 50 / 168),
-        painter: _KeyboardPainter(colors),
+        painter: _KeyboardPainter(colors, defaultColor),
       );
     });
   }
@@ -38,8 +40,9 @@ class KeyboardChordChart extends StatelessWidget {
 
 class _KeyboardPainter extends CustomPainter {
   final List<Color?> noteColors;
+  Color defaultColor = Colors.transparent;
 
-  _KeyboardPainter(this.noteColors) {
+  _KeyboardPainter(this.noteColors, this.defaultColor) {
     assert(noteColors.length == 24);
   }
 
@@ -51,14 +54,14 @@ class _KeyboardPainter extends CustomPainter {
       ..color = Colors.black;
     Paint fill_painter = Paint()..style = PaintingStyle.fill;
 
-    fill_painter.color = noteColors[0] ?? Colors.transparent;
+    fill_painter.color = noteColors[0] ?? defaultColor;
     canvas.drawRect(Rect.fromLTWH(0, 0, size.width * 0.06845238, size.height),
         fill_painter);
 
     canvas.drawRect(Rect.fromLTWH(0, 0, size.width * 0.06845238, size.height),
         stroke_painter);
 
-    fill_painter.color = noteColors[2] ?? Colors.transparent;
+    fill_painter.color = noteColors[2] ?? defaultColor;
     canvas.drawRect(
         Rect.fromLTWH(
             size.width * 0.06845238, 0, size.width * 0.07142857, size.height),
@@ -69,7 +72,7 @@ class _KeyboardPainter extends CustomPainter {
             size.width * 0.06845238, 0, size.width * 0.07142857, size.height),
         stroke_painter);
 
-    fill_painter.color = noteColors[4] ?? Colors.transparent;
+    fill_painter.color = noteColors[4] ?? defaultColor;
     canvas.drawRect(
         Rect.fromLTWH(
             size.width * 0.1398810, 0, size.width * 0.06845238, size.height),
@@ -80,7 +83,7 @@ class _KeyboardPainter extends CustomPainter {
             size.width * 0.1398810, 0, size.width * 0.06845238, size.height),
         stroke_painter);
 
-    fill_painter.color = noteColors[5] ?? Colors.transparent;
+    fill_painter.color = noteColors[5] ?? defaultColor;
     canvas.drawRect(
         Rect.fromLTWH(
             size.width * 0.2083333, 0, size.width * 0.07142857, size.height),
@@ -91,7 +94,7 @@ class _KeyboardPainter extends CustomPainter {
             size.width * 0.2083333, 0, size.width * 0.07142857, size.height),
         stroke_painter);
 
-    fill_painter.color = noteColors[7] ?? Colors.transparent;
+    fill_painter.color = noteColors[7] ?? defaultColor;
     canvas.drawRect(
         Rect.fromLTWH(
             size.width * 0.2797619, 0, size.width * 0.07440476, size.height),
@@ -102,7 +105,7 @@ class _KeyboardPainter extends CustomPainter {
             size.width * 0.2797619, 0, size.width * 0.07440476, size.height),
         stroke_painter);
 
-    fill_painter.color = noteColors[9] ?? Colors.transparent;
+    fill_painter.color = noteColors[9] ?? defaultColor;
     canvas.drawRect(
         Rect.fromLTWH(
             size.width * 0.3541667, 0, size.width * 0.07440476, size.height),
@@ -113,7 +116,7 @@ class _KeyboardPainter extends CustomPainter {
             size.width * 0.3541667, 0, size.width * 0.07440476, size.height),
         stroke_painter);
 
-    fill_painter.color = noteColors[11] ?? Colors.transparent;
+    fill_painter.color = noteColors[11] ?? defaultColor;
     canvas.drawRect(
         Rect.fromLTWH(
             size.width * 0.4285714, 0, size.width * 0.07142857, size.height),
@@ -179,7 +182,7 @@ class _KeyboardPainter extends CustomPainter {
             size.height * 0.6000000),
         stroke_painter);
 
-    fill_painter.color = noteColors[12] ?? Colors.transparent;
+    fill_painter.color = noteColors[12] ?? defaultColor;
     canvas.drawRect(
         Rect.fromLTWH(
             size.width * 0.5000000, 0, size.width * 0.06845238, size.height),
@@ -190,7 +193,7 @@ class _KeyboardPainter extends CustomPainter {
             size.width * 0.5000000, 0, size.width * 0.06845238, size.height),
         stroke_painter);
 
-    fill_painter.color = noteColors[14] ?? Colors.transparent;
+    fill_painter.color = noteColors[14] ?? defaultColor;
     canvas.drawRect(
         Rect.fromLTWH(
             size.width * 0.5684524, 0, size.width * 0.07142857, size.height),
@@ -201,7 +204,7 @@ class _KeyboardPainter extends CustomPainter {
             size.width * 0.5684524, 0, size.width * 0.07142857, size.height),
         stroke_painter);
 
-    fill_painter.color = noteColors[16] ?? Colors.transparent;
+    fill_painter.color = noteColors[16] ?? defaultColor;
     canvas.drawRect(
         Rect.fromLTWH(
             size.width * 0.6398810, 0, size.width * 0.06845238, size.height),
@@ -212,7 +215,7 @@ class _KeyboardPainter extends CustomPainter {
             size.width * 0.6398810, 0, size.width * 0.06845238, size.height),
         stroke_painter);
 
-    fill_painter.color = noteColors[17] ?? Colors.transparent;
+    fill_painter.color = noteColors[17] ?? defaultColor;
     canvas.drawRect(
         Rect.fromLTWH(
             size.width * 0.7083333, 0, size.width * 0.07142857, size.height),
@@ -223,7 +226,7 @@ class _KeyboardPainter extends CustomPainter {
             size.width * 0.7083333, 0, size.width * 0.07142857, size.height),
         stroke_painter);
 
-    fill_painter.color = noteColors[19] ?? Colors.transparent;
+    fill_painter.color = noteColors[19] ?? defaultColor;
     canvas.drawRect(
         Rect.fromLTWH(
             size.width * 0.7797619, 0, size.width * 0.07440476, size.height),
@@ -234,7 +237,7 @@ class _KeyboardPainter extends CustomPainter {
             size.width * 0.7797619, 0, size.width * 0.07440476, size.height),
         stroke_painter);
 
-    fill_painter.color = noteColors[21] ?? Colors.transparent;
+    fill_painter.color = noteColors[21] ?? defaultColor;
     canvas.drawRect(
         Rect.fromLTWH(
             size.width * 0.8541667, 0, size.width * 0.07440476, size.height),
@@ -245,7 +248,7 @@ class _KeyboardPainter extends CustomPainter {
             size.width * 0.8541667, 0, size.width * 0.07440476, size.height),
         stroke_painter);
 
-    fill_painter.color = noteColors[23] ?? Colors.transparent;
+    fill_painter.color = noteColors[23] ?? defaultColor;
     canvas.drawRect(
         Rect.fromLTWH(
             size.width * 0.9285714, 0, size.width * 0.07142857, size.height),
